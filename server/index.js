2c277ea5f8b04df410e445abc23cc873b21b2183
aasthas09 <singhaastha009@gmail.com>
2021-07-02 22:04:43 +0530

    Second Phase

--- server/index.js
+++ server/index.js
@@ -17,11 +17,11 @@ app.use("/posts", postRoutes);
 
 
 
-const CONNECTION_URL = 'mongodb+srv://aasthas09:aasthas09@social-media.gb3to.mongodb.net/posts?retryWrites=true&w=majority';
+
 
 const PORT = process.env.PORT ||5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT,()=>console.log(`Server running on port : ${PORT}`)))
 .catch((error) => console.log(error));
 
